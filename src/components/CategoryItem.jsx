6d55d0fbaf9f2e65f05ad14fd9126a3060e50f57
f1nch <f.socy10@gmail.com>
2022-05-06 03:22:39 +0200

    modified categories cards

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -5,15 +5,36 @@ const Container = styled.div`
   flex:1;
   margin: 3px;
   height: 70vh;
+  position: relative;
 `
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
 `
-const Info = styled.div``
-const Title = styled.h1``
-const Button = styled.button``
+const Info = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const Title = styled.h1`
+  color: white;
+  margin: 20px;
+`
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  font-weight: 500;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+`
 
 const CategoryItem = ({item}) => {
   return (
