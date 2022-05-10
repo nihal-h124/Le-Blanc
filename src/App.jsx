bc7eefe063ce50374ff288610ad7f43b8d3ce688
f1nch <f.socy10@gmail.com>
2022-05-10 20:13:42 +0200

    ProductItem template ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
+// import Home from './pages/Home';
+// import ProductList from './pages/ProductList';
 import ProductItem from './pages/ProductItem';
 
 function App() {
